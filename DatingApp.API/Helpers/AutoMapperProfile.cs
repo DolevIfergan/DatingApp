6d0c95cc8c52a33be0c8cef00b1cb96c302eede7
dolevifergan <dolevifergan@gmail.com>
2019-09-30 10:15:42 +0300

    UpdatingUser

--- DatingApp.API/Helpers/AutoMapperProfile.cs
+++ DatingApp.API/Helpers/AutoMapperProfile.cs
@@ -14,6 +14,7 @@ namespace DatingApp.API.Helpers
             CreateMap<User, UserForDetailedDTO>().ForMember( dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url)).ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge( )));
 
             CreateMap<Photo, PhotoForDetailedDTO>();
+            CreateMap<UserForUpdateDTO, User>();
         }
     }
 }
